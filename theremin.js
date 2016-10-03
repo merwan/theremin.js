4b7c54ed01952cddad0f56cbf4278d81bf148865
Merouane Atig <merouane@teezily.com>
2016-10-03 21:00:41 +0200

    Basic (and buggy) implementation of theremin

--- theremin.js
+++ theremin.js
@@ -1,1 +1,25 @@
-console.log("OK!");
+let audioContext = new AudioContext();
+console.log(audioContext);
+
+let oscillator = audioContext.createOscillator();
+let gain = audioContext.createGain();
+oscillator.connect(gain);
+gain.connect(audioContext.destination);
+oscillator.start();
+oscillator.suspend();
+
+let play = true;
+
+document.addEventListener('click', function(event) {
+  if (play) {
+    oscillator.resume();
+  } else {
+    oscillator.suspend();
+  }
+  play = !play;
+});
+
+document.addEventListener('mousemove', function(event) {
+  oscillator.frequency.value = event.clientX;
+  gain.gain.value = event.clientY / 1000;
+});
