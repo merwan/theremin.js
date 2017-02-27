01917d413cd52a1722a61377084333ad445b816c
Merouane Atig <merwan@patem.net>
2017-02-27 22:14:19 +0100

    Handle start/stop

--- theremin.js
+++ theremin.js
@@ -1,25 +1,36 @@
+const WIDTH = window.innerWidth;
+const HEIGHT = window.innerHeight;
+
+const MIN_FREQ = 20.0;
+const MAX_FREQ = 2000.0;
+
+const FREQ_RATIO = MAX_FREQ / WIDTH;
+const GAIN_RATIO = 1.0 / HEIGHT;
+
 let audioContext = new AudioContext();
-console.log(audioContext);
 
 let oscillator = audioContext.createOscillator();
 let gain = audioContext.createGain();
 oscillator.connect(gain);
 gain.connect(audioContext.destination);
 oscillator.start();
-oscillator.suspend();
 
-let play = true;
+let play = false;
 
 document.addEventListener('click', function(event) {
   if (play) {
-    oscillator.resume();
+    gain.connect(audioContext.destination);
   } else {
-    oscillator.suspend();
+    gain.disconnect(audioContext.destination);
   }
   play = !play;
 });
 
+function changeSound(xpos, ypos) {
+  oscillator.frequency.value = MIN_FREQ + xpos * FREQ_RATIO;
+  gain.gain.value = ypos * GAIN_RATIO;
+}
+
 document.addEventListener('mousemove', function(event) {
-  oscillator.frequency.value = event.clientX;
-  gain.gain.value = event.clientY / 1000;
+  changeSound(event.clientX, event.clientY);
 });
