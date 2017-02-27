d769b5f758e53bcf9d21f7f2470d8555c071baea
Merouane Atig <merwan@patem.net>
2017-02-27 22:48:02 +0100

    Add analyser and empty canvas

--- theremin.js
+++ theremin.js
@@ -7,17 +7,20 @@ const MAX_FREQ = 2000.0;
 const FREQ_RATIO = MAX_FREQ / WIDTH;
 const GAIN_RATIO = 1.0 / HEIGHT;
 
-let audioContext = new AudioContext();
+let audioContext = new (window.AudioContext || window.webkitAudioContext)();
+
 
 let oscillator = audioContext.createOscillator();
+let analyser = audioContext.createAnalyser();
 let gain = audioContext.createGain();
-oscillator.connect(gain);
+oscillator.connect(analyser);
+analyser.connect(gain);
 gain.connect(audioContext.destination);
 oscillator.start();
 
 let play = false;
 
-document.addEventListener('click', function(event) {
+document.addEventListener('click', function() {
   if (play) {
     gain.connect(audioContext.destination);
   } else {
@@ -34,3 +37,9 @@ function changeSound(xpos, ypos) {
 document.addEventListener('mousemove', function(event) {
   changeSound(event.clientX, event.clientY);
 });
+
+let canvas = document.querySelector('#oscilloscope');
+var canvasContext = canvas.getContext('2d');
+canvasContext.clearRect(0, 0, WIDTH, HEIGHT);
+canvasContext.fillStyle = 'rgb(200, 200, 200)';
+canvasContext.fillRect(0, 0, WIDTH, HEIGHT);
